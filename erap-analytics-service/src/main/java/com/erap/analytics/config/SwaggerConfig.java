97881ac62dab03ca41a17e7635ebdc6bacb9994f
Shivam Agrawal <shivamagrawal1166171@gmail.com>
2026-06-19 20:26:57 +0530

    feat(analytics-svc): add Swagger config and OpenAPI definition

---
+++ erap-analytics-service/src/main/java/com/erap/analytics/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.erap.analytics.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT")
+public class SwaggerConfig {
+}
